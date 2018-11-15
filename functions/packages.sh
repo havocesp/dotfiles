258da08844be978638a3685f007a1502dfdb2159
Daniel J. Umpierrez <umpierrez@pm.me>
2018-11-15 16:41:31 +0100

    Commented code removed and pip and pip3 functions merged to just one

--- functions/packages.sh
+++ functions/packages.sh
@@ -73,97 +73,24 @@ function get_module_dirs() {
 	fi
 }
 
-# function get_class_module() {
-# 
-# 	if [ $# -gt 0 ]; then
-# 		local pydir result filter
-# 		local -a modules
-# 		filter="$(echo -n '(from[a-zA-Z0-9_ ]+)?(import[ ]+)('"${1}"')')"
-# 
-# 		for pydir in $(get_python_dirs "${@:2}"); do
-# 			result="$(grep -REo "$filter" "${pydir}" 2>/dev/null | cut -f1 -d':' | sed 's|.*lib/python.*/dist-packages/||g' | cut -f1 -d'/')"
-# 			# echo "$pydir: $result"
-# 			[ -n "$result" ] && modules+="$result"
-# 		done
-# 
-# 		if [ "${#modules[@]}" -gt 0 ]; then
-# 			result="$(get_most_common "${modules[@]}")"
-# 			if [ -n "$result" ]; then
-# 				echo "$result"
-# 				return 0
-# 			else
-# 				return 1
-# 			fi
-# 		else
-# 			return 1
-# 		fi
-# 	else
-# 		return 16
-# 	fi
-# 
-# }
-
-# function pyimporter() {
-# 	local pyfile mod
-# 	if [ $# -ge 1 ]; then
-# 		for pyfile in "${@}"; do
-# 			if [ -f "${pyfile}" ] && [ -r "${pyfile}" ]; then
-# 				mod="$(grep -oE '^(import|from)[ ]+[a-zA-Z0-9_\.]+' "${pyfile}" | cut -d' ' -f2 | grep -v '__')"
-# 				if [ -n "$mod" ]; then
-# 
-# 					is_pip_installed "$mod" || test -n "$(is_pip_installable "$mod")" && pip install --user "$(is_pip_installable "$mod")"
-# 					is_pip3_installed "$mod" || test -n "$(is_pip3_installable "$mod")" && pip3 install --user "$(is_pip3_installable "$mod")"
-# 
-# 				else
-# 
-# 					return 1
-# 				fi
-# 			fi
-# 		done
-# 	else
-# 		return 16
-# 	fi
-# }
-
-# === PIP RELATED ===
-
-function pip_ensure_installed() {
-	if [ $# -eq 1 ]; then
+function is_pip_installed() {
+    local version
+    version=
+	if [[ $# -ge 1  ]]; then
+	    if echo "$1" == "2" ]];then
+	        version="2.7"
 		get_pip_installed | grep -q "$1"
 	else
 		return 16
 	fi
 }
 
-function is_pip3_installed() {
-	if [ $# -eq 1 ]; then
-		get_pip3_installed | grep -q "$1"
-	else
-		return 16
-	fi
-}
-
-# function is_pip3_installable() {
-# 	if [ $# -eq 1 ]; then
-# 		pip3 search "$1" | cut -d' ' -f1 | sort | grep "^$1" | head -n1
-# 	else
-# 		return 16
-# 	fi
-# }
-
 function get_pip_installed() {
-	pip list --format=freeze | cut -d'=' -f1
+	pip$1 list --format=freeze | cut -d'=' -f1
 }
 
-function get_pip3_installed() {
-	pip3 list --format=freeze | cut -d'=' -f1
-}
 function get_pip_outdated() {
-	pip list --format=freeze --outdated | cut -d'=' -f1
-}
-
-function get_pip3_outdated() {
-	pip3 list --format=freeze --outdated | cut -d'=' -f1
+	pip$1 list --format=freeze --outdated | cut -d'=' -f1
 }
 
 # === APT RELATED ===
