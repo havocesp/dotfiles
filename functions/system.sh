d3380e8e802202309d4ad398a2e4c0a4572aaf61
Daniel J. Umpierrez <umpierrez@pm.me>
2019-07-28 13:30:56 +0200

    Removed repeated comments

--- functions/system.sh
+++ functions/system.sh
@@ -18,23 +18,3 @@ function load_distro_info() {
     source /etc/lsb-release
   fi
 }
-#!/usr/bin/env bash
-# ================================
-# Author: Daniel J. Umpierrez
-# --------------------------------
-# Modified: 01-09-2018 00:21
-# ================================
-
-function load_distro_info() {
-  if [ -f "/etc/os-release" ]; then
-    source /etc/os-release
-    export OS_LIKE_CODENAME="${UBUNTU_CODENAME}"
-    export OS_CODENAME="${VERSION_CODENAME}"
-    export OS_ID="${ID}"
-    export OS_NAME="${NAME}"
-    export OS_VERSION_ID="${VERSION_ID}"
-    export OS_ID_LIKE="${ID_LIKE}"
-  elif [ -f "/etc /etc/lsb-release" ]; then 
-    source /etc/lsb-release
-  fi
-}
